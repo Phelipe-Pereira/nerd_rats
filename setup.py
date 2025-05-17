43af0ba22b4d431e50d526c6915293e444453207
Phelipe Leandro Pereira <phelipe.pereira0811@hotmail.com>
2025-05-17 16:12:32 -0300

    chore: remove arquivos de cache e ajusta configurações

--- setup.py
+++ setup.py
@@ -4,9 +4,5 @@ setup(
     name="nerd_rats",
     version="0.1",
     packages=find_packages(),
-    install_requires=[
-        "pynput>=1.7.6",
-        "requests>=2.31.0",
-        "python-dotenv>=1.0.0"
-    ],
-) 
+    install_requires=["pynput>=1.7.6", "requests>=2.31.0", "python-dotenv>=1.0.0"],
+)
